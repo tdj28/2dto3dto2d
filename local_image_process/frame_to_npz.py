3f22e14ebde224cb05818f300dc3003d7d851495
tdj28 <ti.do.jo@gmail.com>
2023-10-07 19:48:00 -0700

    chore: add exception handling

--- local_image_process/frame_to_npz.py
+++ local_image_process/frame_to_npz.py
@@ -111,74 +111,76 @@ def process_frame_to_npz(
     model = model.to(device)
 
     while True:
-        if frame_extraction_complete.is_set() and input_img_queue.empty():
-            break
-        
-        frame_index, total_frames, frame_data, write_to_file, outfile_path = input_img_queue.get()
-        logger.debug(f"Got frame {frame_index} from queue for converstion to NPZ obj")
-        if frame_data is None:
-            logger.info("Exit received in process_frame_to_npz.")  # Changed log message
-            break  
-        # frame_path, frame_num, num_frames = frame_data
-        #logger.info(f"Processing frame {frame_num}")
-        if write_to_file:
-            image = Image.open(outfile_path)
-        else:
-            image = Image.fromarray(frame_data)
+        try:
+            if frame_extraction_complete.is_set() and input_img_queue.empty():
+                break
+            
+            frame_index, total_frames, frame_data, write_to_file, outfile_path = input_img_queue.get()
+            logger.debug(f"Got frame {frame_index} from queue for converstion to NPZ obj")
+            if frame_data is None:
+                logger.info("Exit received in process_frame_to_npz.")  # Changed log message
+                break  
+            # frame_path, frame_num, num_frames = frame_data
+            #logger.info(f"Processing frame {frame_num}")
+            if write_to_file:
+                image = Image.open(outfile_path)
+            else:
+                image = Image.fromarray(frame_data)
 
-        new_height = 480 if image.height > 480 else image.height
-        new_height -= (new_height % 32)
-        new_width = int(new_height * image.width / image.height)
-        diff = new_width % 32
-        #print("b")
+            new_height = 480 if image.height > 480 else image.height
+            new_height -= (new_height % 32)
+            new_width = int(new_height * image.width / image.height)
+            diff = new_width % 32
+            #print("b")
 
-        new_width = new_width - diff if diff < 16 else new_width + 32 - diff
-        new_size = (new_width, new_height)
-        image = image.resize(new_size)
-        #print("bb")
-        #print(type(image), image.size)
-        inputs = feature_extractor(images=image, return_tensors="pt")
-        inputs = inputs.to(device)
-        #print("c")
+            new_width = new_width - diff if diff < 16 else new_width + 32 - diff
+            new_size = (new_width, new_height)
+            image = image.resize(new_size)
+            #print("bb")
+            #print(type(image), image.size)
+            inputs = feature_extractor(images=image, return_tensors="pt")
+            inputs = inputs.to(device)
+            #print("c")
 
-        with torch.no_grad():
-            outputs = model(**inputs)
-            predicted_depth = outputs.predicted_depth
+            with torch.no_grad():
+                outputs = model(**inputs)
+                predicted_depth = outputs.predicted_depth
 
-        pad = 16
-        output = predicted_depth.squeeze().cpu().numpy() * 1000.0
-        output = output[pad:-pad, pad:-pad]
-        image = image.crop((pad, pad, image.width - pad, image.height - pad))
+            pad = 16
+            output = predicted_depth.squeeze().cpu().numpy() * 1000.0
+            output = output[pad:-pad, pad:-pad]
+            image = image.crop((pad, pad, image.width - pad, image.height - pad))
 
-        width, height = image.size
-        #print(f"{width} {height}")
-        depth_image = (output * 255 / np.max(output)).astype('uint8')
-        image = np.array(image)
-        depth_o3d = o3d.geometry.Image(depth_image)
-        image_o3d = o3d.geometry.Image(image)
-        rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(image_o3d, depth_o3d, convert_rgb_to_intensity=False)
-        camera_intrinsic = o3d.camera.PinholeCameraIntrinsic()
-        camera_intrinsic.set_intrinsics(width, height, 500, 500, width/2, height/2)
-        pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, camera_intrinsic)
+            width, height = image.size
+            #print(f"{width} {height}")
+            depth_image = (output * 255 / np.max(output)).astype('uint8')
+            image = np.array(image)
+            depth_o3d = o3d.geometry.Image(depth_image)
+            image_o3d = o3d.geometry.Image(image)
+            rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(image_o3d, depth_o3d, convert_rgb_to_intensity=False)
+            camera_intrinsic = o3d.camera.PinholeCameraIntrinsic()
+            camera_intrinsic.set_intrinsics(width, height, 500, 500, width/2, height/2)
+            pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, camera_intrinsic)
 
-        # Extract point coordinates and colors from Open3D point cloud
-        points = np.asarray(pcd.points)
-        colors = np.asarray(pcd.colors)
-        
-        # Save to npz
-        npz_path = None
-        if write_to_file:
-            npz_path = outfile_path.replace('png', 'npz')
-            np.savez(npz_path, points=points, colors=colors)
-            logger.info(f"Successfully saved NPZ file at {npz_path}")
-            points = None
-            colors = None
-        #npz_path = frame_path.replace('./media/input_frames', './media/npz_files').replace('.png', '.npz')  # Make sure this directory exists or adjust accordingly
-        #np.savez(npz_path, points=points, colors=colors)
-        #logger.info(f"Successfully saved NPZ file at {npz_path}")
+            # Extract point coordinates and colors from Open3D point cloud
+            points = np.asarray(pcd.points)
+            colors = np.asarray(pcd.colors)
+            
+            # Save to npz
+            npz_path = None
+            if write_to_file:
+                npz_path = outfile_path.replace('png', 'npz')
+                np.savez(npz_path, points=points, colors=colors)
+                logger.info(f"Successfully saved NPZ file at {npz_path}")
+                points = None
+                colors = None
 
-        # Camera Position Parameterization (remains unchanged)
-        #eye_x, eye_y, eye_z = 0, -0.4, -0.8
-        npz_queue.put((points, colors, frame_index, total_frames, write_to_file, npz_path)) # (npz_path, eye_x, eye_y, eye_z))  # Queue npz file and camera position for next stage
+            npz_queue.put((points, colors, frame_index, total_frames, write_to_file, npz_path)) # (npz_path, eye_x, eye_y, eye_z))  # Queue npz file and camera position for next stage
     
-    npz_extraction_complete.set()  # Maybe consider renaming this flag to something more generic like 'extraction_complete'
+        except Exception as e:
+            logger.error(f"Error processing frame {frame_index}: {e}")
+            continue  # Skip to the next frame if an error occurs
+    try:   
+        npz_extraction_complete.set() 
+    except Exception as e:
+        logger.error(f"Error processing frame {frame_index}: {e}")
--- main.py
+++ main.py
@@ -14,99 +14,75 @@ def ensure_directory_exists(directory_path):
         os.makedirs(directory_path)
 
 def main():
-    multiprocessing.set_start_method('spawn', force=True)
-
-    # input_video_path = './input_video.mp4'
-    # input_frames_path = './media/input_frames'
-    # output_frames_path = './media/output_frames'
-    # output_video_path = './output_video.mp4'
-    # frame_filename_glob_string = '%08d'
-
-    logger = setup_logger('2dto3dto2d')
-
-    write_to_file = False
-    output_dir = None
-
-    # write_to_file = True
-    # output_dir = './media/'
-    # ensure_directory_exists(output_dir)
-
-    ensure_directory_exists('./media')
-    input_video_path = './media/input_video.mp4'
-    output_video_path = './media/output_video.mp4'
-
-    num_cpus = multiprocessing.cpu_count()
-
-
-    frame_extraction_complete = multiprocessing.Event()
-    npz_extraction_complete = [multiprocessing.Event() for _ in range(num_cpus)]
-    image_processing_complete = [multiprocessing.Event() for _ in range(num_cpus)]
-    final_video_creation_complete = multiprocessing.Event()
-
-    input_img_queue = multiprocessing.Queue()
-    npz_queue = multiprocessing.Queue()
-    output_image_queue = multiprocessing.Queue()
-
-    # Start processes
-    processes = [
-        multiprocessing.Process(target=extract_frames, args=(input_video_path, input_img_queue, frame_extraction_complete, write_to_file, output_dir)),
-    ]
-
-    for i in range(num_cpus):
-        processes.append(multiprocessing.Process(target=process_frame_to_npz, args=(input_img_queue, npz_queue, frame_extraction_complete, npz_extraction_complete[i])))
-        processes.append(multiprocessing.Process(target=process_npz_to_image, args=(npz_queue, output_image_queue, npz_extraction_complete, image_processing_complete[i])))
-
-    processes.append(multiprocessing.Process(target=collect_and_write_images, args=(output_image_queue, image_processing_complete, final_video_creation_complete, output_video_path)))
-
-    # # Start processes
-    # processes = [
-    #     multiprocessing.Process(target=extract_frames, args=(input_video_path, input_img_queue, frame_extraction_complete, write_to_file, output_dir)),
-    #     multiprocessing.Process(target=process_frame_to_npz, args=(input_img_queue, npz_queue, frame_extraction_complete, npz_extraction_complete)),
-    #     multiprocessing.Process(target=process_frame_to_npz, args=(input_img_queue, npz_queue, frame_extraction_complete, npz_extraction_complete)),
-    #     multiprocessing.Process(target=process_frame_to_npz, args=(input_img_queue, npz_queue, frame_extraction_complete, npz_extraction_complete)),
-    #     multiprocessing.Process(target=process_npz_to_image, args=(npz_queue, output_image_queue, frame_extraction_complete, npz_extraction_complete, image_processing_complete)),
-    #     multiprocessing.Process(target=process_npz_to_image, args=(npz_queue, output_image_queue, frame_extraction_complete, npz_extraction_complete, image_processing_complete)),
-    #     multiprocessing.Process(target=process_npz_to_image, args=(npz_queue, output_image_queue, frame_extraction_complete, npz_extraction_complete, image_processing_complete)),
-    #     multiprocessing.Process(target=collect_and_write_images, args=(output_image_queue, frame_extraction_complete, npz_extraction_complete, image_processing_complete, final_video_creation_complete, output_video_path)),
-    #     # multiprocessing.Process(target=process_ply_to_image, args=(ply_queue, frame_extraction_complete, ply_extraction_complete, image_processing_complete, logger)),
-    # ]
-    for p in processes:
-        p.start()
-
-    # Monitor frame directory and queue new frames
-    # known_frames = set()
-    # frame_num = 0  # Initialize frame number
-
-    # while not frame_extraction_complete.is_set():
-    #     current_frames = set(glob.glob(f"{input_frames_path}/*.png"))
-
-    #     # #print(current_frames)
-    #     # new_frames = current_frames - known_frames
-    #     # num_frames = len(current_frames)  # Update total number of frames
-    #     # for frame_path in new_frames:
-    #     #     frame_num += 1  # Increment frame number
-    #     #     frame_queue.put((frame_path, frame_num, num_frames))  # Updated line
-    #     #     #print(frame_queue.qsize())
-    #     # #print(f"Queued {len(new_frames)} new frames")
-    #     # known_frames.update(new_frames)
-    #     if frame_extraction_complete.is_set() and not new_frames:
-    #         frame_queue.put(None)  # Signal ply processing to exit once all frames have been queued
-    #     time.sleep(1)  # Wait before checking for new frames again
-
-
-    final_video_creation_complete.wait()
-    # # Signal ply processing to exit
-    # frame_queue.put(None)
-
-    # Wait for all processes to finish
-    for p in processes:
-        p.join()
-
     
-    # Create video from output images
-    #create_video(output_frames_path, output_video_path)
+    try:
+        multiprocessing.set_start_method('spawn', force=True)
+
+        logger = setup_logger('2dto3dto2d')
+        logger.info("Starting main function")
+
+
+        write_to_file = False
+        output_dir = None
+
+        ensure_directory_exists('./media')
+        logger.debug("Ensured media directory exists")
+
+
+        input_video_path = './media/input_video.mp4'
+        output_video_path = './media/output_video.mp4'
+
+        # Validate inputs
+        if not os.path.isfile(input_video_path):
+            logger.error(f"Input video file does not exist: {input_video_path}")
+            return
+        else:
+            logger.info(f"Input video file exists: {input_video_path}")
+
+
+        num_cpus = multiprocessing.cpu_count()
+        logger.info(f"Number of CPUs: {num_cpus}")
+
+
+        frame_extraction_complete = multiprocessing.Event()
+        npz_extraction_complete = [multiprocessing.Event() for _ in range(num_cpus)]
+        image_processing_complete = [multiprocessing.Event() for _ in range(num_cpus)]
+        final_video_creation_complete = multiprocessing.Event()
+
+        input_img_queue = multiprocessing.Queue()
+        npz_queue = multiprocessing.Queue()
+        output_image_queue = multiprocessing.Queue()
+
+        # Start processes
+        processes = [
+            multiprocessing.Process(target=extract_frames, args=(input_video_path, input_img_queue, frame_extraction_complete, write_to_file, output_dir)),
+        ]
+
+        #for i in range(num_cpus):
+        for i in range(max(1, num_cpus//2)):
+            processes.append(multiprocessing.Process(target=process_frame_to_npz, args=(input_img_queue, npz_queue, frame_extraction_complete, npz_extraction_complete[i])))
+            processes.append(multiprocessing.Process(target=process_npz_to_image, args=(npz_queue, output_image_queue, npz_extraction_complete, image_processing_complete[i])))
+
+        processes.append(multiprocessing.Process(target=collect_and_write_images, args=(output_image_queue, image_processing_complete, final_video_creation_complete, output_video_path)))
+
+        for p in processes:
+            p.start()
+
+        final_video_creation_complete.wait()
+
+        # Wait for all processes to finish
+        for p in processes:
+            p.join()
+
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        return
+
 
 if __name__ == '__main__':
     if not torch.cuda.is_available():
         sys.exit("Error: CUDA is not available.")
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"An error occurred: {e}")
